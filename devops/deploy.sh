01a7b7ba23ec4414d4c7bf946a6e8413cfa885bf
Justin Merz <jrmerz@gmail.com>
2024-11-15 14:38:24 -0800

    switching to cork-app build

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -4,6 +4,7 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $ROOT_DIR
 
 gcloud config set project digital-ucdavis-edu
+REGISTRY=us-west1-docker.pkg.dev/digital-ucdavis-edu/pub
 
 ./cloudbuild.sh
 
@@ -14,10 +15,9 @@ fi
 
 echo "Deploying to Google Cloud..."
 gcloud run deploy client-error-reporter \
-  --image gcr.io/digital-ucdavis-edu/client-error-reporter:$IMAGE_TAG \
+  --image $REGISTRY/client-error-reporter:$IMAGE_TAG \
   --platform managed \
   --region us-west1 \
   --set-env-vars=ALLOWED_DOMAINS_PATH=/config/domains/domains.txt,SERVER_KEY_PATH=/config/key/key.txt \
   --set-secrets="/config/domains/domains.txt=client-error-reporter-domains:latest,/config/key/key.txt=client-error-reporter-key:latest" \
   --allow-unauthenticated
-
